ea78c8f04b0f481493d648527b2857dd72288346
andrej <andrej.km997@gmail.com>
2020-01-06 12:03:10 +0100

    i ovo

--- Asteroid.py
+++ Asteroid.py
@@ -16,6 +16,7 @@ isDestroyedThisObj1 = 0
 tmpss = 0
 
 
+
 class Asteroid(QLabel):
     def __init__(self, w, h, scene: QGraphicsScene, uniqueIdenfier):
         super().__init__()
@@ -58,104 +59,108 @@ class Asteroid(QLabel):
         global coordinatesOfRocketsss
         global levelOfAsteroid
         global isDestroyedThisObj1
-        if Server.activeAsteroids[self.uniqueIdenfier] == 0:
-            ast_0 += 1
-            # if(numberObj == 3):
-            # numberObj = 0
-            ast_0 = ast_0 % 4
-            #        print(ast_0)
-            startPath = "Images/AsteroidsImg/large/c100"
-            endPath = ".png"
-            # if(numberObj % 2 == 0):
-            #     i = (i - 1) % 16
-            # else:
-            i = (i + 1) % 16
-            if i < 10:
-                fullPath = (startPath + '0' + str(i) + endPath)
+
+        if Server.create == False:
+            if Server.activeAsteroids[self.uniqueIdenfier] == 0:
+                ast_0 += 1
+                # if(numberObj == 3):
+                # numberObj = 0
+                ast_0 = ast_0 % 4
+                #        print(ast_0)
+                startPath = "Images/AsteroidsImg/large/c100"
+                endPath = ".png"
+                # if(numberObj % 2 == 0):
+                #     i = (i - 1) % 16
+                # else:
+                i = (i + 1) % 16
+                if i < 10:
+                    fullPath = (startPath + '0' + str(i) + endPath)
+                else:
+                    fullPath = (startPath + str(i) + endPath)
+
+                cropedImage = self.setAssteroidSize(fullPath)
+                self.setImage(cropedImage)
+                if (ast_0 % 2 == 0):
+                    self.angle = self.angle - ast_0
+                else:
+                    self.angle = self.angle + ast_0
+
+                self.moveX = cos(radians(self.angle))
+                self.moveY = sin(radians(self.angle))
+                self.yFull = float(self.yFull).__sub__(self.moveY * 6)
+                self.xFull = float(self.xFull).__add__(self.moveX * 9)
+                self.move(self.xFull, self.yFull)
+
+                #region unistavanje asteroida
+                inttX = int(round(self.xFull))
+                inttY = int(round(self.yFull))
+
+                ccc = 0
+                vvv = 0
+                tmpss = 0
+                thisAsteroidXCoords = []
+                thisAsteroidYCoords = []
+
+                thisAsteroidXCoords.clear()
+                thisAsteroidYCoords.clear()
+                for ccc in range(20):
+                    tmpss = inttX + ccc
+                    thisAsteroidXCoords.append(tmpss)
+                    ccc = ccc + 1
+                for vvv in range(20):
+                    tmpss2 = inttY + vvv
+                    thisAsteroidYCoords.append(tmpss2)
+                    vvv = vvv + 1
+                vvv = 0
+
+                if (any(checkXCords in thisAsteroidXCoords for checkXCords in Server.coordinatesOfRocketsX) and any(
+                        checkYCords in thisAsteroidYCoords for checkYCords in Server.coordinatesOfRocketsY)):
+                    Server.player1Lives = Server.player1Lives - 1
+                    self.myScene.label2.setText("Player1 lives--->[" + Server.player1Lives.__str__() + "] score--->[" + Server.player1Score.__str__() + "]")
+                    Server.activeAsteroids[self.uniqueIdenfier] = 1
+                    self.hide()
+                    print("ASTEROID IS DESTROYED TOO!!!")
+                    if Server.player1Lives == 0:#ako je izgubio sve zivote da iskoci iz igrce
+                        self.myScene.game_is_over()
+                #endRegion
+
+                expandBulletX = []
+                expandBulletY = []
+
+                for key, value in Server.bulletsCollectionX.items():
+                    omega = 0
+                    for omega in range(5):
+                        expandBulletX.append(omega + value)
+                        omega = omega + 1
+                    if any(cx in expandBulletX for cx in thisAsteroidXCoords):
+                        for key2, val2 in Server.bulletsCollectionY.items():
+                            omega2 = 0
+                            for omega2 in range(5):
+                                expandBulletY.append(omega2 + val2)
+                                omega2 = omega2 + 1
+                            if any(cy in expandBulletY for cy in thisAsteroidYCoords) and key == key2:
+                                Server.activeAsteroids[self.uniqueIdenfier] = 1
+                                Server.player1Score = Server.player1Score + 300
+                                self.myScene.label2.setText("Player1 lives--->[" + Server.player1Lives.__str__() + "] score--->[" + Server.player1Score.__str__() + "]")
+                                self.hide()
+                        break
+                expandBulletY.clear()
+                expandBulletX.clear()
+
+                if (math.floor(self.yFull) <= -10):
+                    self.yFull = 500
+                elif (math.floor(self.yFull) >= 500):
+                    self.yFull = 0
+                elif (math.floor(self.xFull) <= -22):
+                    self.xFull = 559
+                elif (math.floor(self.xFull) >= 600):
+                    self.xFull = -21.0
             else:
-                fullPath = (startPath + str(i) + endPath)
-
-            cropedImage = self.setAssteroidSize(fullPath)
-            self.setImage(cropedImage)
-            if (ast_0 % 2 == 0):
-                self.angle = self.angle - ast_0
-            else:
-                self.angle = self.angle + ast_0
-
-            self.moveX = cos(radians(self.angle))
-            self.moveY = sin(radians(self.angle))
-            self.yFull = float(self.yFull).__sub__(self.moveY * 6)
-            self.xFull = float(self.xFull).__add__(self.moveX * 9)
-            self.move(self.xFull, self.yFull)
-
-            #region unistavanje asteroida
-            inttX = int(round(self.xFull))
-            inttY = int(round(self.yFull))
-
-            ccc = 0
-            vvv = 0
-            tmpss = 0
-            thisAsteroidXCoords = []
-            thisAsteroidYCoords = []
-
-            thisAsteroidXCoords.clear()
-            thisAsteroidYCoords.clear()
-            for ccc in range(20):
-                tmpss = inttX + ccc
-                thisAsteroidXCoords.append(tmpss)
-                ccc = ccc + 1
-            for vvv in range(20):
-                tmpss2 = inttY + vvv
-                thisAsteroidYCoords.append(tmpss2)
-                vvv = vvv + 1
-            vvv = 0
-
-            if (any(checkXCords in thisAsteroidXCoords for checkXCords in Server.coordinatesOfRocketsX) and any(
-                    checkYCords in thisAsteroidYCoords for checkYCords in Server.coordinatesOfRocketsY)):
-                Server.player1Lives = Server.player1Lives - 1
-                self.myScene.label2.setText("Player1 lives--->[" + Server.player1Lives.__str__() + "] score--->[" + Server.player1Score.__str__() + "]")
-                Server.activeAsteroids[self.uniqueIdenfier] = 1
-                self.hide()
-                print("ASTEROID IS DESTROYED TOO!!!")
-                if Server.player1Lives == 0:#ako je izgubio sve zivote da iskoci iz igrce
-                    self.myScene.game_is_over()
-            #endRegion
-
-            expandBulletX = []
-            expandBulletY = []
-
-            for key, value in Server.bulletsCollectionX.items():
-                omega = 0
-                for omega in range(5):
-                    expandBulletX.append(omega + value)
-                    omega = omega + 1
-                if any(cx in expandBulletX for cx in thisAsteroidXCoords):
-                    for key2, val2 in Server.bulletsCollectionY.items():
-                        omega2 = 0
-                        for omega2 in range(5):
-                            expandBulletY.append(omega2 + val2)
-                            omega2 = omega2 + 1
-                        if any(cy in expandBulletY for cy in thisAsteroidYCoords) and key == key2:
-                            Server.activeAsteroids[self.uniqueIdenfier] = 1
-                            Server.player1Score = Server.player1Score + 300
-                            self.myScene.label2.setText("Player1 lives--->[" + Server.player1Lives.__str__() + "] score--->[" + Server.player1Score.__str__() + "]")
-                            self.hide()
-                    break
-            expandBulletY.clear()
-            expandBulletX.clear()
-
-            if (math.floor(self.yFull) <= -10):
-                self.yFull = 500
-            elif (math.floor(self.yFull) >= 500):
-                self.yFull = 0
-            elif (math.floor(self.xFull) <= -22):
-                self.xFull = 559
-            elif (math.floor(self.xFull) >= 600):
-                self.xFull = -21.0
+                self.yFull = 50000
+                self.xFull = 19052
         else:
-            self.yFull = 50000
-            self.xFull = 19052
+            o = 0
+            # ovde treba da se vrate koordinate
 
     def timerEvent(self, a0: 'QTimerEvent'):
         self.whileTrue()
-
